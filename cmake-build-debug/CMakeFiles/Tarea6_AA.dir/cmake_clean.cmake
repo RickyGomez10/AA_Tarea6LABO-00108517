d4c1beca9c2beb903ebf11221eec90bb302897f5
RickyGomez10 <00108517@uca.edu.sv>
2019-10-22 01:15:49 -0600

    Tarea 6 labo

---
+++ cmake-build-debug/CMakeFiles/Tarea6_AA.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tarea6_AA.dir/main.cpp.obj"
+  "Tarea6_AA.pdb"
+  "Tarea6_AA.exe"
+  "Tarea6_AA.exe.manifest"
+  "libTarea6_AA.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tarea6_AA.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Tarea6_AA.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Tarea6_AA.
+# This may be replaced when dependencies are built.
